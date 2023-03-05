fbc4c8102e7161846a5472bcccb7294d7e5c29d6
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-05 17:12:33 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Matheus Viana on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
