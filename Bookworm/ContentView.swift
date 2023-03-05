c349e0b3867faec79e60afa74d538f52d61c06d2
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-05 17:25:11 -0300

    use @Binding to propagate the internal value to external state

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct PushButton: View {
     let title: String
-    @State var isOn: Bool
+    @Binding var isOn: Bool
     
     var onColors = [Color.red, Color.yellow]
     var offColors = [Color(white: 0.6), Color(white: 0.4)]
@@ -33,7 +33,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            PushButton(title: "Remember Me", isOn: rememberMe)
+            PushButton(title: "Remember Me", isOn: $rememberMe)
             Text(rememberMe ? "On" : "Off")
         }
     }
