4d5366b36eaf853465e038426c7e0ad0a1fff145
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-06 23:31:12 -0300

    Create rating system

--- Bookworm/RatingView.swift
+++ Bookworm/RatingView.swift
@@ -21,7 +21,25 @@ struct RatingView: View {
     
     var body: some View {
         HStack {
+            if !label.isEmpty {
+                Text(label)
+            }
             
+            ForEach(1..<maximumRating + 1, id: \.self) { number in
+                image(for: number)
+                    .foregroundColor(number > rating ? offColor : onColor)
+                    .onTapGesture {
+                        rating = number
+                    }
+            }
+        }
+    }
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
         }
     }
 }
