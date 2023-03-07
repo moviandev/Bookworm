d2364def7b461761da162fd67e37fc58e434210b
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-06 23:27:53 -0300

    create rating component

---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,33 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Matheus Viana on 06/03/23.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+    
+    var label = ""
+    var maximumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView(rating: .constant(4))
+    }
+}
