05d4173ec03571c9eb4a629412cf0b91794b576b
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-07 22:52:57 -0300

    Add book details in detail view

--- Bookworm/DetailView.swift
+++ Bookworm/DetailView.swift
@@ -11,6 +11,32 @@ struct DetailView: View {
     let book: Book
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.74))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y:-5)
+            }
+            
+            Text(book.author ?? "Unknowon Author")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(book.review ?? "No review")
+                .padding()
+            
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title ?? "Unknown Book")
     }
 }
