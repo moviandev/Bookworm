57434ff41c00d41beaf55d40c2e2a2af2309ea52
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-07 23:15:16 -0300

    Add onDelete for deleting books in our list

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -35,9 +35,14 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBook)
             }
             .navigationTitle("Bookworm")
             .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         showingAddScreen.toggle()
@@ -50,6 +55,15 @@ struct ContentView: View {
                 AddBookView()
             }
         }
+    }
+    
+    func deleteBook(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        
+        try? moc.save()
     }
 }
 
