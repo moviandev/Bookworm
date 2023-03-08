8f94197386874498fbdc4a349aeeebedd05e571b
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-07 22:47:10 -0300

    organize file order, create detailView and remove preview cuz it's took much work to make it work with Core Data, and add files in assets

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,16 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Matheus Viana on 07/03/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
