a8d7e0d4c7733f7fc037be33920bc8c27932ce43
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-07 23:29:10 -0300

    Dismissing navigationLink programmatically and deleting book from the detailView

--- Bookworm/DetailView.swift
+++ Bookworm/DetailView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct DetailView: View {
     let book: Book
     
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    @State private var showingDeleteAlert = false
+    
     var genre: String {
         if book.genre == nil {
             return "Fantasy"
@@ -50,5 +54,26 @@ struct DetailView: View {
                 .font(.largeTitle)
         }
         .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)
+        .alert("Delete book?", isPresented: $showingDeleteAlert) {
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            Button {
+                showingDeleteAlert = true
+            } label: {
+                Label("Delete this book", systemImage: "trash.fill")
+            }
+        }
+    }
+    
+    func deleteBook() {
+        moc.delete(book)
+        
+        try? moc.save()
+        dismiss()
     }
 }
